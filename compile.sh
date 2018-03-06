bbb70923929cf49a25f87d094755d4915fd9be7b
Sven Pachnit <sven@bmonkeys.net>
2018-03-06 10:38:39 +0100

    Initial release

---
+++ compile.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+../scripting/spcomp -i addons/sourcemod/scripting/include -i ../scripting/include -o addons/sourcemod/plugins/csgo_webshortcuts.smx addons/sourcemod/scripting/csgo_webshortcuts.sp
